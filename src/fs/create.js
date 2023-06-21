d01f4a610fc9339d9c9327c9a218e689a1554128
andre353 <eva595eva@gmail.com>
2023-06-21 21:34:47 +0300

    fix: add absolute file path in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import {writeFile} from 'node:fs/promises';
 
 const create = async () => {
 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const filePath = './files/fresh.txt';
+    const absoluteFilePath = path.join(__dirname, filePath);
+
     const fileContent = 'I am fresh and young';
     const errorMessage = new Error('FS operation failed'); 
     
     try {
-        await writeFile(filePath, fileContent, { flag: 'wx' });
+        await writeFile(absoluteFilePath, fileContent, { flag: 'wx' });
     } catch (error) {
         if (error.code === 'EEXIST') {
             throw errorMessage;            
